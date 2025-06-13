b4f03eeb86691517abdaa974e5e4f78581f7a5a6
WelRaj <rajkumar.rathor.250925@gmail.com>
2025-06-13 23:24:40 +0530

    commit

---
+++ chapter6/conditional.py
@@ -0,0 +1,8 @@
+a=int(input("enter your age:"))
+if(a>=18):
+    print("raj is valid for this work")
+elif(a<0):
+    print("enter valid age:")
+
+else:
+    print("you are not valid for that work")
