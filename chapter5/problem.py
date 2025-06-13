0165ff3a7897a953a3d506112bd1928cf126c111
WelRaj <rajkumar.rathor.250925@gmail.com>
2025-06-13 11:58:19 +0530

    your message

---
+++ chapter5/problem.py
@@ -0,0 +1,26 @@
+
+#1
+''''words={
+    "madad":"help",
+    "kursi":"chair",
+    "billi":"cat"
+}
+word=input("Enter the words you want meaning of:")
+print(words[word])'''
+#2
+s=set()
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+n=input("enter number :")
+s.add(int(n))
+print(s)
