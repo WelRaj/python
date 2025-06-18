d78d1921b32041ca2c232902c57aea9962235dbe
WelRaj <rajkumar.rathor.250925@gmail.com>
2025-06-18 16:39:35 +0530

    commit

--- chapter10/class.py
+++ chapter10/class.py
@@ -1,6 +1,14 @@
-class Employee:
+'''class Employee:
     name ="Raj"
     language="python"
     salary=1200000
 harry=Employee()
+print(harry.name,harry.language,harry.salary)'''
+class Employee:
+    language="python" #class attributes
+    salary=1400000
+harry=Employee()
+harry.name="rajkumar" #object attributes or instance attributes
+print(harry.name,harry.language,harry.salary)
+harry.name="ajay"
 print(harry.name,harry.language,harry.salary)
